3d69d3c9d1a7d03a282ad5bb535b0bd6010a7157
Zyad <49781778+sudo-sturbia@users.noreply.github.com>
2020-04-20 20:28:30 +0200

    Create Request to represent CRUD functions.

---
+++ src/main/java/com/github/sudo_sturbia/agatha/server/request/Request.java
@@ -0,0 +1,29 @@
+package com.github.sudo_sturbia.agatha.server.request;
+
+/**
+ * Request represents a client request to be handled by server.
+ * <p>
+ * Classes implementing Request should both verify the correctness
+ * of the Request and handle its execution.
+ * <p>
+ * Request defines four constants representing the four CRUD functions,
+ * each having a related class implementing its verification/handling.
+ */
+public interface Request
+{
+    /**
+     * Constants representing CRUD functions.
+     */
+    public enum Function
+    {
+        CREATE, READ, UPDATE, DELETE
+    }
+
+    /**
+     * Handle client's request and generate a response string.
+     *
+     * @return A response to the request based on the function.
+     *         Responses are documented in implementing classes.
+     */
+    public String handle();
+}
