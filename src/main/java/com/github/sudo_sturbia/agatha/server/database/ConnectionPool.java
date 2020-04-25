607e9da0c58af18f9043b07089208eaefc265b69
Zyad A <49781778+sudo-sturbia@users.noreply.github.com>
2020-04-25 22:09:57 +0200

    Create driver connector and initial connections pool.
    Created two implementations of Connector interface:
    
    - DriverConnector is a simple wrapper around DriverManager.getConnection
    - ConnectionPool is meant to be a implementation that supports pooling,
    still to be implemented.

---
+++ src/main/java/com/github/sudo_sturbia/agatha/server/database/ConnectionPool.java
@@ -0,0 +1,49 @@
+package com.github.sudo_sturbia.agatha.server.database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * ConnectionPool is an implementation of Connector that provides
+ * a pool of connections for usage.
+ */
+public class ConnectionPool implements Connector
+{
+    /** Name of Application's database. */
+    private final String dbName;
+
+    /** Username for database server (MySQL). */
+    private final String dbServerUsername;
+
+    /** Password for database server (MySQL). */
+    private final String dbServerPass;
+
+    /**
+     * DriverConnector's constructor.
+     *
+     * @param dbName name of database.
+     * @param dbServerUsername username for database server (MySQL.)
+     * @param dbServerPass password for database server (MySQL.)
+     */
+    public ConnectionPool(String dbName, String dbServerUsername, String dbServerPass)
+    {
+        this.dbName = dbName;
+        this.dbServerUsername = dbServerUsername;
+        this.dbServerPass = dbServerPass;
+    }
+    @Override
+    public void setup() throws SQLException
+    {
+    }
+
+    @Override
+    public Connection get() throws SQLException
+    {
+        return null;
+    }
+
+    @Override
+    public void close(Connection connection) throws SQLException
+    {
+    }
+}
---
+++ src/main/java/com/github/sudo_sturbia/agatha/server/database/DriverConnector.java
@@ -0,0 +1,54 @@
+package com.github.sudo_sturbia.agatha.server.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * DriverConnector is a wrapper around DriverManager to use
+ * Connector interface.
+ */
+public class DriverConnector implements Connector
+{
+    /** Name of Application's database. */
+    private final String dbName;
+
+    /** Username for database server (MySQL). */
+    private final String dbServerUsername;
+
+    /** Password for database server (MySQL). */
+    private final String dbServerPass;
+
+    /**
+     * DriverConnector's constructor.
+     *
+     * @param dbName name of database.
+     * @param dbServerUsername username for database server (MySQL.)
+     * @param dbServerPass password for database server (MySQL.)
+     */
+    public DriverConnector(String dbName, String dbServerUsername, String dbServerPass)
+    {
+        this.dbName = dbName;
+        this.dbServerUsername = dbServerUsername;
+        this.dbServerPass = dbServerPass;
+    }
+
+    @Override
+    public void setup() throws SQLException
+    {
+        // no setup needed ..
+    }
+
+    @Override
+    public Connection get() throws SQLException
+    {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/" + this.dbName,
+                this.dbServerUsername, this.dbServerPass);
+    }
+
+    @Override
+    public void close(Connection connection) throws SQLException
+    {
+        connection.close();
+    }
+}
