f89944548358657fde5aaa3c0c827ac410d51b16
Zyad A. Ali <zyad.ali.me@gmail.com>
2020-06-27 15:33:20 +0200

    UI: Create a book tab to display client's books.

---
+++ client/src/main/java/com/github/sudo_sturbia/agatha/client/controller/BookController.java
@@ -0,0 +1,38 @@
+package com.github.sudo_sturbia.agatha.client.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class BookController
+{
+    @FXML
+    private Button updateName;
+
+    @FXML
+    private Label nameField;
+
+    @FXML
+    private Button updateAuthor;
+
+    @FXML
+    private Label authorField;
+
+    @FXML
+    private Button updateState;
+
+    @FXML
+    private Label stateField;
+
+    @FXML
+    private Button updatePages;
+
+    @FXML
+    private Label pagesField;
+
+    @FXML
+    private Button updateReadPages;
+
+    @FXML
+    private Label readPagesField;
+}
