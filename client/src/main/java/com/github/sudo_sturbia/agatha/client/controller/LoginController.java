97810d6d0a4c2b82416d74ef33f5aa68c01fa048
Zyad A. Ali <zyad.ali.me@gmail.com>
2020-06-28 09:57:33 +0200

    UI: Create a user login layout.

---
+++ client/src/main/java/com/github/sudo_sturbia/agatha/client/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.github.sudo_sturbia.agatha.client.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/** Controller of the login display. */
+public class LoginController
+{
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+    public void login(ActionEvent actionEvent) {
+
+    }
+}
